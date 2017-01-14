39c19dfb5ad435461306666f2d2b624c58a22abd
Benjamin Brown <benjaminmbrown@Benjamins-MacBook-Air.local>
2017-01-13 21:05:02 -0600

    Basic interpolation expressions

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,11 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector:'app-root',
   styleUrls: ['app.component.scss'],
-  template:` <div class="app">{{title}}</div> `
+  template:` 
+  <div class="app">
+  <div>{{title + '!' }}</div>
+  <div> {{numberOne}}</div>
+  <div>  {{numberTwo}}</div>
+  <div> {{numberOne + numberTwo}} </div>
+  <div> {{isHappy ? ':)':':('}} </div>
+  </div> 
+  `
 })
 export class AppComponent {
   title:string;
-
+  numberOne:number = 1;
+  numberTwo:number = 2;
+  isHappy:boolean = false;
   constructor(){
     this.title = "Basic Angular App";
   }
