58d1ec12edbad914edf6243f812798c08e7d3e7b
Benjamin Brown <benjaminmbrown@Benjamins-MacBook-Air.local>
2017-01-13 21:00:49 -0600

    Bootstrapping angular

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector:'app-root',
-  template:` <div>{{title}}</div> `
+  styleUrls: ['app.component.scss'],
+  template:` <div class="app">{{title}}</div> `
 })
 export class AppComponent {
   title:string;
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,19 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { CommonModule } from '@angular/common';
-
-import { AppComponent } from './app.component';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {CommonModule} from '@angular/common';
+import {AppComponent} from './app.component';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    CommonModule
-  ],
-  bootstrap: [
+  declarations:[
     AppComponent
-  ],
-  declarations: [
+    ],
+    imports:[
+      BrowserModule,
+      CommonModule
+    ],
+  bootstrap:[
     AppComponent
-  ]
+    ]
 })
-export class AppModule {}
+
+export class AppModule{
+
+}
